a2a5511d5c4588881e34562a5561bd2b65b65a2a
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2019-09-24 18:30:52 +0200

    [DEV] SEOS-585: Added const to all parameters where possible

--- SeosCryptoApi.h
+++ SeosCryptoApi.h
@@ -18,19 +18,19 @@
  *
  */
 seos_err_t
-SeosCryptoApi_rngGetBytes(SeosCryptoCtx*        cryptoCtx,
-                          unsigned int          flags,
+SeosCryptoApi_rngGetBytes(SeosCryptoCtx*        ctx,
+                          const unsigned int    flags,
                           void*                 buf,
-                          size_t                bufSize);
+                          const size_t          bufSize);
 
 /**
  * @brief Reseed the internal RNG
  *
  */
 seos_err_t
-SeosCryptoApi_rngReSeed(SeosCryptoCtx*      cryptoCtx,
+SeosCryptoApi_rngReSeed(SeosCryptoCtx*      ctx,
                         const void*         seed,
-                        size_t              seedLen);
+                        const size_t        seedLen);
 
 // ------------------------------ Digest API -----------------------------------
 
@@ -39,37 +39,37 @@ SeosCryptoApi_rngReSeed(SeosCryptoCtx*      cryptoCtx,
  *
  */
 seos_err_t
-SeosCryptoApi_digestInit(SeosCryptoCtx*                 cryptoCtx,
-                         SeosCrypto_DigestHandle*       pDigestHandle,
-                         unsigned int                   algorithm);
+SeosCryptoApi_digestInit(SeosCryptoCtx*             ctx,
+                         SeosCrypto_DigestHandle*   pDigestHandle,
+                         const unsigned int         algorithm);
 
 /**
  * @brief Finish a digest object
  *
  */
 seos_err_t
-SeosCryptoApi_digestFree(SeosCryptoCtx*                cryptoCtx,
-                         SeosCrypto_DigestHandle       digestHandle);
+SeosCryptoApi_digestFree(SeosCryptoCtx*                ctx,
+                         const SeosCrypto_DigestHandle digestHandle);
 
 /**
  * @brief Add data to a digest
  *
  */
 seos_err_t
-SeosCryptoApi_digestUpdate(SeosCryptoCtx*               cryptoCtx,
-                           SeosCrypto_DigestHandle      digestHandle,
-                           const void*                  data,
-                           size_t                       dataLen);
+SeosCryptoApi_digestUpdate(SeosCryptoCtx*                   ctx,
+                           const SeosCrypto_DigestHandle    digestHandle,
+                           const void*                      data,
+                           const size_t                     dataLen);
 
 /**
  * @brief Write resulting digest to buffer
  *
  */
 seos_err_t
-SeosCryptoApi_digestFinalize(SeosCryptoCtx*             cryptoCtx,
-                             SeosCrypto_DigestHandle    digestHandle,
-                             void*                      digest,
-                             size_t*                    digestSize);
+SeosCryptoApi_digestFinalize(SeosCryptoCtx*                 ctx,
+                             const SeosCrypto_DigestHandle  digestHandle,
+                             void*                          digest,
+                             size_t*                        digestSize);
 
 // -------------------------------- Key API ------------------------------------
 
@@ -80,9 +80,9 @@ SeosCryptoApi_digestFinalize(SeosCryptoCtx*             cryptoCtx,
 seos_err_t
 SeosCryptoApi_keyInit(SeosCryptoCtx*                   ctx,
                       SeosCrypto_KeyHandle*            keyHandle,
-                      unsigned int                     type,
-                      unsigned int                     flags,
-                      size_t                           bits);
+                      const unsigned int               type,
+                      const unsigned int               flags,
+                      const size_t                     bits);
 
 /**
  * @brief Generate key with internal RNG
@@ -90,16 +90,16 @@ SeosCryptoApi_keyInit(SeosCryptoCtx*                   ctx,
  */
 seos_err_t
 SeosCryptoApi_keyGenerate(SeosCryptoCtx*               ctx,
-                          SeosCrypto_KeyHandle         keyHandle);
+                          const SeosCrypto_KeyHandle   keyHandle);
 
 /**
  * @brief Generate a private/public keypair with internal RNG
  *
  */
 seos_err_t
-SeosCryptoApi_keyGeneratePair(SeosCryptoCtx*           ctx,
-                              SeosCrypto_KeyHandle     prvKeyHandle,
-                              SeosCrypto_KeyHandle     pubKeyHandle);
+SeosCryptoApi_keyGeneratePair(SeosCryptoCtx*                ctx,
+                              const SeosCrypto_KeyHandle    prvKeyHandle,
+                              const SeosCrypto_KeyHandle    pubKeyHandle);
 
 /**
  * @brief Import key data into key object from buffer
@@ -107,10 +107,10 @@ SeosCryptoApi_keyGeneratePair(SeosCryptoCtx*           ctx,
  */
 seos_err_t
 SeosCryptoApi_keyImport(SeosCryptoCtx*                 ctx,
-                        SeosCrypto_KeyHandle           keyHandle,
-                        SeosCrypto_KeyHandle           wrapKeyHandle,
+                        const SeosCrypto_KeyHandle     keyHandle,
+                        const SeosCrypto_KeyHandle     wrapKeyHandle,
                         const void*                    keyBytes,
-                        size_t                         keySize);
+                        const size_t                   keySize);
 
 /**
  * @brief Export key data from key object into buffer
@@ -118,8 +118,8 @@ SeosCryptoApi_keyImport(SeosCryptoCtx*                 ctx,
  */
 seos_err_t
 SeosCryptoApi_keyExport(SeosCryptoCtx*                 ctx,
-                        SeosCrypto_KeyHandle           keyHandle,
-                        SeosCrypto_KeyHandle           wrapKeyHandle,
+                        const SeosCrypto_KeyHandle     keyHandle,
+                        const SeosCrypto_KeyHandle     wrapKeyHandle,
                         void*                          buf,
                         size_t*                        bufSize);
 
@@ -128,8 +128,8 @@ SeosCryptoApi_keyExport(SeosCryptoCtx*                 ctx,
  *
  */
 seos_err_t
-SeosCryptoApi_keyFree(SeosCryptoCtx*                 ctx,
-                      SeosCrypto_KeyHandle           keyHandle);
+SeosCryptoApi_keyFree(SeosCryptoCtx*                ctx,
+                      const SeosCrypto_KeyHandle    keyHandle);
 
 // ----------------------------- Signature API ---------------------------------
 
@@ -140,41 +140,41 @@ SeosCryptoApi_keyFree(SeosCryptoCtx*                 ctx,
 seos_err_t
 SeosCryptoApi_signatureInit(SeosCryptoCtx*                ctx,
                             SeosCrypto_SignatureHandle*   pSigHandle,
-                            unsigned int                  algorithm,
-                            SeosCrypto_KeyHandle          prvHandle,
-                            SeosCrypto_KeyHandle          pubHandle);
+                            const unsigned int            algorithm,
+                            const SeosCrypto_KeyHandle    prvHandle,
+                            const SeosCrypto_KeyHandle    pubHandle);
 
 /**
  * @brief Finish a signature object
  *
  */
 seos_err_t
-SeosCryptoApi_signatureFree(SeosCryptoCtx*               ctx,
-                            SeosCrypto_SignatureHandle   sigHandle);
+SeosCryptoApi_signatureFree(SeosCryptoCtx*                      ctx,
+                            const SeosCrypto_SignatureHandle    sigHandle);
 
 /**
  * @brief Sign a hash value
  *
  */
 seos_err_t
-SeosCryptoApi_signatureSign(SeosCryptoCtx*                 ctx,
-                            SeosCrypto_SignatureHandle     sigHandle,
-                            const void*                    hash,
-                            size_t                         hashSize,
-                            void*                          signature,
-                            size_t*                        signatureSize);
+SeosCryptoApi_signatureSign(SeosCryptoCtx*                      ctx,
+                            const SeosCrypto_SignatureHandle    sigHandle,
+                            const void*                         hash,
+                            const size_t                        hashSize,
+                            void*                               signature,
+                            size_t*                             signatureSize);
 
 /**
  * @brief Verify signature over a hash
  *
  */
 seos_err_t
-SeosCryptoApi_signatureVerify(SeosCryptoCtx*                 ctx,
-                              SeosCrypto_SignatureHandle     sigHandle,
-                              const void*                    hash,
-                              size_t                         hashSize,
-                              const void*                    signature,
-                              size_t                         signatureSize);
+SeosCryptoApi_signatureVerify(SeosCryptoCtx*                    ctx,
+                              const SeosCrypto_SignatureHandle  sigHandle,
+                              const void*                       hash,
+                              const size_t                      hashSize,
+                              const void*                       signature,
+                              const size_t                      signatureSize);
 
 // ----------------------------- Agreement API ---------------------------------
 
@@ -183,29 +183,29 @@ SeosCryptoApi_signatureVerify(SeosCryptoCtx*                 ctx,
  *
  */
 seos_err_t
-SeosCryptoApi_agreementInit(SeosCryptoCtx*                cryptoCtx,
+SeosCryptoApi_agreementInit(SeosCryptoCtx*                ctx,
                             SeosCrypto_AgreementHandle*   pAgrHandle,
-                            unsigned int                  algorithm,
-                            SeosCrypto_KeyHandle          prvHandle);
+                            const unsigned int            algorithm,
+                            const SeosCrypto_KeyHandle    prvHandle);
 
 /**
  * @brief Finish a (key) agreement object
  *
  */
 seos_err_t
-SeosCryptoApi_agreementFree(SeosCryptoCtx*               cryptoCtx,
-                            SeosCrypto_AgreementHandle   agrHandle);
+SeosCryptoApi_agreementFree(SeosCryptoCtx*                      ctx,
+                            const SeosCrypto_AgreementHandle    agrHandle);
 
 /**
  * @brief Agree on a shared key
  *
  */
 seos_err_t
-SeosCryptoApi_agreementAgree(SeosCryptoCtx*                 cryptoCtx,
-                             SeosCrypto_AgreementHandle     agrHandle,
-                             SeosCrypto_KeyHandle           pubHandle,
-                             void*                          shared,
-                             size_t*                        sharedSize);
+SeosCryptoApi_agreementAgree(SeosCryptoCtx*                     ctx,
+                             const SeosCrypto_AgreementHandle   agrHandle,
+                             const SeosCrypto_KeyHandle         pubHandle,
+                             void*                              shared,
+                             size_t*                            sharedSize);
 
 // ------------------------------- Cipher API ----------------------------------
 
@@ -213,51 +213,51 @@ SeosCryptoApi_agreementAgree(SeosCryptoCtx*                 cryptoCtx,
  * @brief Initialize a cipher object
  */
 seos_err_t
-SeosCryptoApi_cipherInit(SeosCryptoCtx*                 cryptoCtx,
+SeosCryptoApi_cipherInit(SeosCryptoCtx*                 ctx,
                          SeosCrypto_CipherHandle*       pCipherHandle,
-                         unsigned int                   algorithm,
-                         SeosCrypto_KeyHandle           keyHandle,
+                         const unsigned int             algorithm,
+                         const SeosCrypto_KeyHandle     keyHandle,
                          const void*                    iv,
-                         size_t                         ivLen);
+                         const size_t                   ivLen);
 
 /**
  * @brief Finish cipher object
  *
  */
 seos_err_t
-SeosCryptoApi_cipherFree(SeosCryptoCtx*                cryptoCtx,
-                         SeosCrypto_CipherHandle       cipherHandle);
+SeosCryptoApi_cipherFree(SeosCryptoCtx*                 ctx,
+                         const SeosCrypto_CipherHandle  cipherHandle);
 
 /**
  * @brief Encrypt data in chunks
  *
  */
 seos_err_t
-SeosCryptoApi_cipherUpdate(SeosCryptoCtx*               cryptoCtx,
-                           SeosCrypto_CipherHandle      cipherHandle,
-                           const void*                  data,
-                           size_t                       dataLen,
-                           void*                        output,
-                           size_t*                      outputSize);
+SeosCryptoApi_cipherUpdate(SeosCryptoCtx*                   ctx,
+                           const SeosCrypto_CipherHandle    cipherHandle,
+                           const void*                      data,
+                           const size_t                     dataLen,
+                           void*                            output,
+                           size_t*                          outputSize);
 
 /**
  * @brief Start encryption of data (only relevant for some algorithms)
  *
  */
 seos_err_t
-SeosCryptoApi_cipherStart(SeosCryptoCtx*               cryptoCtx,
-                          SeosCrypto_CipherHandle      cipherHandle,
-                          const void*                  data,
-                          size_t                       dataLen);
+SeosCryptoApi_cipherStart(SeosCryptoCtx*                ctx,
+                          const SeosCrypto_CipherHandle cipherHandle,
+                          const void*                   data,
+                          const size_t                  dataLen);
 
 /**
  * @brief Finish encryption of data (only relevant for some algorithms)
  *
  */
 seos_err_t
-SeosCryptoApi_cipherFinalize(SeosCryptoCtx*               cryptoCtx,
-                             SeosCrypto_CipherHandle      cipherHandle,
-                             void*                        output,
-                             size_t*                      outputSize);
+SeosCryptoApi_cipherFinalize(SeosCryptoCtx*                 ctx,
+                             const SeosCrypto_CipherHandle  cipherHandle,
+                             void*                          output,
+                             size_t*                        outputSize);
 
 /** @} */
