659852fe1d60cd13a0bfb43b03a4f1a62e0902f9
Leonard <Leonhard.Blazevic@hensoldt-cyber.com>
2019-09-25 18:30:21 +0200

    [DEV] SEOS-581: Adjusting the keystore api according to the new design proposal.

--- SeosKeyStoreApi.h
+++ SeosKeyStoreApi.h
@@ -16,12 +16,9 @@
  * @brief Imports a SeosCrypto_Key object into the keystore
  *
  * @param keyStoreCtx       pointer to keyStoreCtx
- * @param keyHandle         returns the key handle for the imported key
  * @param name              name of the key to import
- * @param keyBytesBuffer    buffer containing the key bytes
- * @param algorithm         algorithm that uses the key
- * @param flags             \see SeosCryptoKey_Flags
- * @param lenBits           length of the key in bits
+ * @param keyData           buffer containing the key data
+ * @param keySize           size of the key data in bytes
  *
  * @retval SEOS_ERROR_INSUFFICIENT_SPACE
  * @retval SEOS_ERROR_INVALID_PARAMETER
@@ -30,18 +27,18 @@
  */
 seos_err_t
 SeosKeyStoreApi_importKey(SeosKeyStoreCtx*          keyStoreCtx,
-                          SeosCrypto_KeyHandle*     keyHandle,
                           const char*               name,
-                          void const*               keyBytesBuffer,
-                          unsigned int              algorithm,
-                          unsigned int              flags,
-                          size_t                    lenBits);
+                          void const*               keyData,
+                          size_t                    keySize);
 /**
  * @brief Retreives the key with a given name from the keystore
  *
- * @param keyStoreCtx   pointer to keyStoreCtx
- * @param keyHandle     receives the key handle
- * @param name          name of the key to get
+ * @param       keyStoreCtx     pointer to keyStoreCtx
+ * @param       name            name of the key to get
+ * @param[out]  keyData         address of the buffer which will be filled 
+ *                              with key data
+ * @param[out]  keySize         address of the variable which will be filled 
+ *                              with key data size
  *
  * @return SEOS_ERROR_GENERIC
  * @retval SEOS_ERROR_INSUFFICIENT_SPACE
@@ -51,33 +48,26 @@ SeosKeyStoreApi_importKey(SeosKeyStoreCtx*          keyStoreCtx,
  */
 seos_err_t
 SeosKeyStoreApi_getKey(SeosKeyStoreCtx*         keyStoreCtx,
-                       SeosCrypto_KeyHandle*    keyHandle,
-                       const char*              name);
+                       const char*              name,
+                       void*                    keyData,
+                       size_t*                  keySize);
 /**
  * @brief Deletes a key with a given name from the keystore
  *
- * @param keyHandle     receives the key handle
+ * @param keyStoreCtx   pointer to keyStoreCtx
+ * @param name          name of the key to delete
  *
  * @return SEOS_ERROR_NOT_FOUND
  *
  */
 seos_err_t
-SeosKeyStoreApi_deleteKey(SeosCrypto_KeyHandle keyHandle);
-
-/**
- * @brief Closes a key with a given handle
- *
- * @param keyHandle     receives the key handle
- *
- * @return SEOS_ERROR_INVALID_HANDLE
- *
- */
-seos_err_t
-SeosKeyStoreApi_closeKey(SeosCrypto_KeyHandle keyHandle);
+SeosKeyStoreApi_deleteKey(SeosKeyStoreCtx*         keyStoreCtx,
+                          const char*              name);
 /**
  * @brief Copies the key from the current key store to the destination key store
  *
- * @param keyHandle     receives the key handle
+ * @param keyStoreCtx   pointer to keyStoreCtx
+ * @param name          name of the key to copy
  * @param destKeyStore  pointer to the destination key store
  *
  * @return SEOS_ERROR_GENERIC
@@ -87,14 +77,16 @@ SeosKeyStoreApi_closeKey(SeosCrypto_KeyHandle keyHandle);
  *
  */
 seos_err_t
-SeosKeyStoreApi_copyKey(SeosCrypto_KeyHandle    keyHandle,
-                        SeosKeyStoreCtx*        destKeyStore);
+SeosKeyStoreApi_copyKey(SeosKeyStoreCtx*         keyStoreCtx,
+                        const char*              name,
+                        SeosKeyStoreCtx*         destKeyStore);
 /**
  * @brief Moves the key from the current key store to
  * the destination key store (after the operation the key is no longer in the
  * current key store)
  *
- * @param keyHandle     receives the key handle
+ * @param keyStoreCtx   pointer to keyStoreCtx
+ * @param name          name of the key to move
  * @param destKeyStore  pointer to the destination key store
  *
  * @return SEOS_ERROR_GENERIC
@@ -104,31 +96,9 @@ SeosKeyStoreApi_copyKey(SeosCrypto_KeyHandle    keyHandle,
  *
  */
 seos_err_t
-SeosKeyStoreApi_moveKey(SeosCrypto_KeyHandle    keyHandle,
-                        SeosKeyStoreCtx*        destKeyStore);
-/**
- * @brief Generates a key with a given name using an RNG, stores the key into the key store
- * and returns the key data in the key object.
- *
- * @param keyStoreCtx   pointer to keyStoreCtx
- * @param keyHandle     returns the key handle for the generated key
- * @param name          name of the key to get
- * @param algorithm     algorithm that uses the key
- * @param flags         \see SeosCryptoKey_Flags
- * @param lenBits       length of the key in bits
- *
- * @retval SEOS_ERROR_INSUFFICIENT_SPACE
- * @retval SEOS_ERROR_INVALID_PARAMETER
- * @retval SEOS_ERROR_OPERATION_DENIED
- *
- */
-seos_err_t
-SeosKeyStoreApi_generateKey(SeosKeyStoreCtx*            keyStoreCtx,
-                            SeosCrypto_KeyHandle*       keyHandle,
-                            const char*                 name,
-                            unsigned int                algorithm,
-                            unsigned int                flags,
-                            size_t                      lenBits);
+SeosKeyStoreApi_moveKey(SeosKeyStoreCtx*         keyStoreCtx,
+                        const char*              name,
+                        SeosKeyStoreCtx*         destKeyStore);
 /**
  * @brief Deletes all the keys from the keystore
  *
