e348f83860fe21f0dfbc4e4ca59c49e4e538089a
Carmelo Pintaudi <carmelo.pintaudi@hensoldt-cyber.com>
2019-12-20 10:47:06 +0100

    [DEV] SEOS-765: provide new commands for CHANMUX_NIC: stop and start

--- include/seos_chanmux_nic.h
+++ include/seos_chanmux_nic.h
@@ -13,5 +13,9 @@ typedef enum
     CHANMUX_NIC_CMD_CLOSE      =  2,   /*!< Close Channel */
     CHANMUX_NIC_RSP_CLOSE_     =  3,   /*!< Close Confirmation */
     CHANMUX_NIC_CMD_GET_MAC    =  4,   /*!< GetMac */
-    CHANMUX_NIC_RSP_GET_MAC    =  5    /*!< GetMac Confirmation */
+    CHANMUX_NIC_RSP_GET_MAC    =  5,   /*!< GetMac Confirmation */
+    CHANMUX_NIC_CMD_START_READ =  6,   /*!< Start reading */
+    CHANMUX_NIC_RSP_START_READ =  7,   /*!< Start reading confirmation*/
+    CHANMUX_NIC_CMD_STOP_READ  =  8,   /*!< Stop reading */
+    CHANMUX_NIC_RSP_STOP_READ  =  9,   /*!< Stop reading confirmation*/
 } seos_chanmux_nic_commands_t;
