a1a649dc3494beca0c9e025413b80a49588a1ee5
Axel Heider <axel.heider@hensoldt-cyber.com>
2019-12-30 04:54:24 +0100

    [DEV] streamlined socket RPC interface

--- include/OS_Network.h
+++ include/OS_Network.h
@@ -61,176 +61,194 @@
 #pragma once
 
 #include "OS_Error.h"
+#include "OS_Types.h"
+#include "SeosError.h"
+#include "seos_types.h"
 #include <stdint.h>
 
 /**
- * @brief   contains elements that must be filled
- *          out by the APP to configure it to listen on a given port.
-*/
-typedef struct
-{
-    unsigned domain;      /**< domain is as of now AF_INET (IPv4)*/
-    unsigned type;        /**< type is as of now SOCK_STREAM (TCP)*/
-    uint16_t listen_port; /**< port to listen to e.g. 5555 */
-    unsigned backlog;     /**< # of connections accepted (currently fixed =1)*/
-} OS_NetworkServer_Socket_t;
+ * @defgroup os_network_api
+ * @file OS_Network.h
+ *
+ * @brief Network stack API.These  API mostly interacts with core SEOS NWStack. \n
+ *        This layer mostly supports all API's for the app for socket related operations.
+ */
+
+
+
+
+// bit 0-3 define the IP protocol version. We stick to RFC791 here, which
+// defines the 'version' field or the IP header to be 4 bits wide. IANA
+// maintains a list at
+// http://www.iana.org/assignments/version-numbers/version-numbers.xhtml
+//
+//   0  reserved (RFC4928)
+//   1  reserved (RFC4928)
+//   2  unassigned
+//   3  unassigned
+//   4  IPv4 (Internet Protocol Version 4, RFC791)
+//   5  ST (ST Datagram Mode, RFC1190
+//   6  IPv6 (Internet Protocol version 6, RFC1752)
+//   7  TP/IX (TP/IX: The Next Internet, RFC1475)
+//   8  PiP (The P Internet Protocol, RFC1621)
+//   9  TUBA, RFC1347)
+//  10  unassigned
+//  11  unassigned
+//  12  unassigned
+//  13  unassigned
+//  14  unassigned
+//  15  reserved
+//
+#define OS_SOCKET_IPV4    (4u << 0)
+#define OS_SOCKET_IPV6    (6u << 0)
+
+#define OS_GET_SOCKET_PROTOCOL_FROM_MODE(mode)    ( (mode) & 0x0F )
+
+// bit 4 defines if stream (TCP) or stream (UDP) is used
+#define OS_SOCKET_STREAM     (0u << 4)
+#define OS_SOCKET_DGRAM      (1u << 4)
+
+#define OS_GET_SOCKET_TYPE_FROM_MODE(mode)        ( (mode) & 0x10 )
+
+
 
 /**
- * @brief   contains elements that must be filled by APP
- *          to connect to a remote host.
+ * @brief   seos_nw_server_struct contains elements that must be filled by APP to configure it
+ *          as server.
+ *
+ * @ingroup seos_nw_api
 */
-
 typedef struct
 {
-    unsigned domain;  /**< domain is as of now AF_INET (IPv4) */
-    unsigned type;    /**< type is as of now SOCK_STREAM (TCP) */
-    const char* name; /**< IP addr to connect to (e.g. "10.0.0.1" ) */
-    uint16_t port;    /**< port is for e.g. HTTP port 80 */
-} OS_Network_Socket_t;
+    unsigned int  mode;
+    unsigned int  backlog;
+    uint16_t      port;
+} OS_server_socket_params_t;
 
-#if !defined(CUSTOM_OS_NETWORK_STACK_API_TYPES)
 
-// Protocol families.
-/** IPv4 */
-#define OS_AF_INET   2 // IP protocol family.
-/** IPv6 */
-#define OS_AF_INET6 10 // IP version 6.
+/**
+ * @brief   seos_nw_client_struct contains elements that must be filled by APP to configure it
+ *          as client.
+ *
+ * @ingroup seos_nw_api
+*/
+typedef struct
+{
+    unsigned int  mode;
+    // RFC1035 limits a fully qualified domain name to 255 characters, where
+    // each node (delimited by a dot) is limited to 63 characters
+    char          name[256];
+    uint16_t      port;
+} OS_client_socket_params_t;
 
-// Socket definition
-/** Stream socket - TCP */
-#define OS_SOCK_STREAM 2
-/** Datagram socket - UDP */
-#define OS_SOCK_DGRAM  1
 
-typedef void* OS_Network_Context_t;
-typedef unsigned OS_NetworkSocket_Handle_t;
-typedef unsigned OS_NetworkServer_Handle_t;
+#define DECLARE_TYPED_SOCKET_SOCKET_API(_type_, _handle_type_t_) \
+    typedef struct \
+    { \
+        seos_err_t (*server_socket_create)( \
+            const OS_server_socket_params_t*  params, \
+            _handle_type_t_*                  pServerSocketHandle); \
+        \
+        seos_err_t (*client_socket_create)(  \
+            const OS_client_socket_params_t*  params,  \
+            _handle_type_t_*                  pClientSocketHandle);  \
+        \
+        seos_err_t (*close)(  \
+            _handle_type_t_  handle);  \
+        \
+        seos_err_t (*accept)( \
+            _handle_type_t_    serverSocketHandle, \
+            _handle_type_t_ *  pClientSocketHandle); \
+         \
+        seos_err_t (*read)( \
+            _handle_type_t_  handle, \
+            size_t *         pLen); \
+         \
+        seos_err_t (*write)( \
+            _handle_type_t_  handle, \
+            size_t *         pLen); \
+        \
+    } _type_;
+
+
+#if !defined(CUSTOM_SEOS_NETWORK_STACK_API_TYPES)
+
+typedef unsigned int  OS_socket_handle_t;
+typedef unsigned int  OS_server_socket_handle_t;
+
+DECLARE_TYPED_SOCKET_SOCKET_API(OS_socket_api_ctx_t, unsigned int);
 
 #endif
 
-/**
- * @details Create a socket and connect to it
- *
- * @param[in] ctx:  Runtime context of the APP. As of now
- * NULL.
- *
- * @param[in] pClientStruct: Configuration parameters for the socket to
- * be created.
- *
- * @param[out] phandle: Handle to the created socket.
- *
- * @return  OS_SUCCESS or OS_ERROR
- */
+
+#define SETUP_SOCKET_SOCKET_API_INSTANCE(_prefix_) \
+    { \
+        .server_socket_create = _prefix_##_server_socket_create, \
+        .client_socket_create = _prefix_##_client_socket_create, \
+        .close                = _prefix_##_socket_close, \
+        .accept               = _prefix_##_socket_accept, \
+        .read                 = _prefix_##_socket_read, \
+        .write                = _prefix_##_socket_write, \
+    }
+
+
+
+typedef struct
+{
+    event_wait_func_t      wait_init_done;
+    OS_socket_api_ctx_t    api;
+    seos_shared_buffer_t   port;
+} OS_network_client_lib_config_t;
+
+
+void
+OS_network_client_lib_init(
+    const OS_network_client_lib_config_t*   cfg);
+
+
+//------------------------------------------------------------------------------
+// SEOS Socket API
+//------------------------------------------------------------------------------
 
 OS_Error_t
-OS_NetworkSocket_create(
-    OS_Network_Context_t       ctx,
-    OS_Network_Socket_t*       pClientStruct,
-    OS_NetworkSocket_Handle_t* phandle);
+OS_client_socket_create(
+    const OS_network_client_lib_config_t*  ctx,
+    const OS_client_socket_params_t*       params,
+    OS_socket_handle_t*                    phandle);
 
-/**
- * @details Create a server socket, binds to a
- * port and listen for incoming connections
- *
- * @param[in] ctx:  Runtime context of the APP. As of now
- * NULL.
- *
- * @param[in] pServerStruct: Configuration parameters for the
- * server socket to be created.
- *
- * @param[out] pSrvHandle: Handle to the created socket.
- *
- * @return  OS_SUCCESS or OS_ERROR
- */
 
 OS_Error_t
-OS_NetworkServerSocket_create(
-    OS_Network_Context_t       ctx,
-    OS_NetworkServer_Socket_t* pServerStruct,
-    OS_NetworkServer_Handle_t* pSrvHandle);
-/**
- * @details Closes a NetworkServer socket. No
- * further socket communication is possible after closure.
- *
- * @param[in] srvHandle: Handle to the server socket we want
- * to close.
- *
- * @return  OS_SUCCESS or OS_ERROR
- */
+OS_server_socket_create(
+    const OS_network_client_lib_config_t*  ctx,
+    const OS_server_socket_params_t*       params,
+    OS_server_socket_handle_t*             pHandle);
+
 
 OS_Error_t
-OS_NetworkServerSocket_close(
-    OS_NetworkServer_Handle_t srvHandle);
+OS_socket_accept(
+    OS_server_socket_handle_t serverSocketHandle,
+    OS_socket_handle_t*       pHandle);
 
-/**
- * @details Closes a network socket. No further
- * socket communication is possible after closure.
- *
- * @param[in] handle: Handle to the socket we want to close.
- *
- * @return OS_SUCCESS or OS_ERROR
- *
- */
 
 OS_Error_t
-OS_NetworkSocket_close(
-    OS_NetworkSocket_Handle_t handle);
+OS_server_socket_close(
+    OS_server_socket_handle_t  handle);
 
-/**
- * @details Writes to a connected network socket.
- *
- * @param[in] handle: Handle to the socket to be used.
- *
- * @param[in] buf: Buffer containing data to be written.
- *
- * @param[in] len: Length of data to be written.
- *
- * @return Actual Number of Bytes written or OS_ERROR
- *
- */
-OS_Error_t
-OS_NetworkSocket_write(
-    OS_NetworkSocket_Handle_t handle,
-    const void*               buf,
-    size_t*                   len);
-
-/**
- * @details Accept incoming connections.
- *
- * @param[in] srvHandle: Handle used to create server socket.
- *
- * @param[out] phSocket: Network Socket containing the data
- * needed for further communication with the connecting host. All subsequent
- * read/write commands in this conversation need this handle.
- *
- * @return OS_SUCCESS or OS_ERROR
- */
 
 OS_Error_t
-OS_NetworkServerSocket_accept(
-    OS_NetworkServer_Handle_t  srvHandle,
-    OS_NetworkSocket_Handle_t* phSocket);
+OS_socket_close(
+    OS_socket_handle_t  handle);
 
-/**
- * @details Read data from connected socket.
-
- * @param[in] handle: Used to create/open socket
-
- * @param[in] buf: Buffer to read data into
- *
- * @param[in,out] len: Indicates how much data to read. After read it indicates how much was actually read
- *
- * @return OS_Error_t, following combinations with value of len
-
- * OS_ERROR_CONNECTION_CLOSED and length = 0, connection closed\n
- * OS_ERROR_GENERIC  error during read\n
- * OS_SUCCESS and len = 0, nothing read, connection still established\n
- * OS_SUCCESS and len > 0, data read, connection still established\n
- */
 
 OS_Error_t
-OS_NetworkSocket_read(
-    OS_NetworkSocket_Handle_t handle,
-    void*                     buf,
-    size_t*                   len);
-/** @} */
+OS_socket_write(
+    OS_socket_handle_t  handle,
+    const void*         buf,
+    size_t*             len);
+
+
+OS_Error_t
+OS_socket_read(
+    OS_socket_handle_t  handle,
+    void*               buf,
+    size_t*             len);
