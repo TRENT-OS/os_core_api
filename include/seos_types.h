c7330dff3aad56ccaaa31f3aadc7660c94515214
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-03-02 18:49:09 +0100

    [DEV] Added mutex lock/unlock function types to seos_types.h

--- include/seos_types.h
+++ include/seos_types.h
@@ -10,6 +10,8 @@
 
 typedef void (*event_wait_func_t)(void);
 typedef void (*event_notify_func_t)(void);
+typedef int (*mutex_lock_func_t)(void);
+typedef int (*mutex_unlock_func_t)(void);
 
 typedef struct
 {
