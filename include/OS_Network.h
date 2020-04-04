a742b4b9bbf84c4f415a9c0426410c91fda108fd
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-04-04 11:24:58 +0200

    [DOC] SEOS-403/SEOS-1004 Update doxygen documentation for Network Stack

--- include/OS_Network.h
+++ include/OS_Network.h
@@ -1,7 +1,61 @@
-/*
- *  OS_Network.h
+/**
+ *  Copyright (C) 2019-2020, Hensoldt Cyber GmbH
  *
- *  Copyright (C) 2019, Hensoldt Cyber GmbH
+ * @file OS_Network.h
+ * @defgroup OS_Network OS Network API
+ * @brief TRENTOS-M Network Socket API. These API interacts with the Network
+ *        Stack to offer IP functionality to the system using a socket based
+ *        interface.
+ * @details Include system header for TRENTOS-M Network Socket API. The
+ *          Network stack can be used in two different ways, in a SERVER
+ *          configuration (where the APP listens for an incoming connection)
+ *          and a CLIENT configuration (where the APP can connect to a remote
+ *          host).\n\n
+ *          The Network Stack is implemented as a CAmkES component and in
+ *          order to use it you must instantiate at least one such component
+ *          and additionally an interface file and a CAmkES component file must
+ *          be created/configured. \n\n The reference implementation can be
+ *          found in the test_network_api folder in tests.\n
+ *
+ *          <b> Dependencies </b> \n
+ *          The Network depends on a network interface driver, the
+ *          configuration server and the system libraries.\n\n
+ *
+ *          <b> Network interface file: </b> \n
+ *          The CAmkES interface implementation can be found in
+ *          if_OS_socket.camkes file.
+ *
+ *          <b> CAmkES configuration file: </b> \n
+ *          In order to use the Stack, your component definition needs to
+ *          include the following.
+ *          \code
+ *              component <COMPONENT_NAME> {
+ *                      //Reference to the RPC socket interface
+ *                      uses      if_OS_socket    network_stack_rpc;
+ *                      //A buffer to send data to/from the stack
+ *                     dataport  Buf             buffer;
+ *                      //Event received when the stack is ready to be used
+ *                      consumes  ServiceReady    event_init_done;
+ *              }
+ *          \endcode
+ *
+ *          <b> CMakeLists.txt file: </b> \n
+ *          The component must be build with either of the 2 "SEOS_NWSTACK_AS_" defines.
+ *          \code
+ *              Define                            | Description
+ *              ----------------------------------|---------------------------------------------------------------------------
+ *               SEOS_NWSTACK_AS_CLIENT           | use the Network stack as client (connect to remote host)
+ *               SEOS_NWSTACK_AS_SERVER           | use the Network stack as server (accept connection from remote host)
+ *               OS_NETWORK_STACK_USE_CONFIGSERVER| use configuration server to configure IP addresses
+ *          \endcode
+ *
+ *          <b> Limitations: </b> \n
+ *          + Only IPv4 connections are supported
+ *          + Only TCP socket is supported
+ *          + Only 1 connection can be active at a given time
+ *          + Each APP needs its own Network Stack
+ *          + Network Stack needs to be specialized in Server/Client
+ * @{
  */
 
 #pragma once
@@ -10,52 +64,42 @@
 #include <stdint.h>
 
 /**
- * @defgroup os_network_api
- * @file OS_Network.h
- *
- * @brief Network stack API.These  API mostly interacts with core SEOS NWStack. \n
- *        This layer mostly supports all API's for the app for socket related operations.
- */
-
-/**
- * @brief   OS_NetworkServer_Socket_t contains elements that must be filled by APP to configure it
-            as server.
-
- * @ingroup os_network_api
-
+ * @brief   contains elements that must be filled
+ *          out by the APP to configure it to listen on a given port.
 */
 typedef struct
 {
-    unsigned domain;      /**< domain is as of now AF_INET */
-    unsigned type;        /**< type is as of now SOCK_STREAM */
+    unsigned domain;      /**< domain is as of now AF_INET (IPv4)*/
+    unsigned type;        /**< type is as of now SOCK_STREAM (TCP)*/
     uint16_t listen_port; /**< port to listen to e.g. 5555 */
-    unsigned backlog;     /**< number of connections accepted (currently fixed =1)*/
+    unsigned backlog;     /**< # of connections accepted (currently fixed =1)*/
 } OS_NetworkServer_Socket_t;
 
 /**
- * @brief   OS_Network_Socket_t contains elements that must be filled by APP to configure it
-            as client.
-
- * @ingroup os_network_api
-
+ * @brief   contains elements that must be filled by APP
+ *          to connect to a remote host.
 */
 
 typedef struct
 {
-    unsigned domain;  /**< domain is as of now AF_INET */
-    unsigned type;    /**< type is as of now SOCK_STREAM */
-    const char* name; /**< name must be IP addr to connect to */
+    unsigned domain;  /**< domain is as of now AF_INET (IPv4) */
+    unsigned type;    /**< type is as of now SOCK_STREAM (TCP) */
+    const char* name; /**< IP addr to connect to (e.g. "10.0.0.1" ) */
     uint16_t port;    /**< port is for e.g. HTTP port 80 */
 } OS_Network_Socket_t;
 
 #if !defined(CUSTOM_OS_NETWORK_STACK_API_TYPES)
 
 // Protocol families.
-#define OS_AF_INET   2   // IP protocol family.
-#define OS_AF_INET6  10 // IP version 6.
+/** IPv4 */
+#define OS_AF_INET   2 // IP protocol family.
+/** IPv6 */
+#define OS_AF_INET6 10 // IP version 6.
 
 // Socket definition
+/** Stream socket - TCP */
 #define OS_SOCK_STREAM 2
+/** Datagram socket - UDP */
 #define OS_SOCK_DGRAM  1
 
 typedef void* OS_Network_Context_t;
@@ -65,57 +109,52 @@ typedef unsigned OS_NetworkServer_Handle_t;
 #endif
 
 /**
- * @details %OS_NetworkSocket_create,  Create a socket and connect to it
-
- * @param OS_Network_Context_t:  Passed by App which is the run time context received by APP. As of now NULL
-
- * @param OS_Network_Socket_t: Must be filled by app. Please see the struct OS_Network_Socket_t for details
-
-          domain(required) -> Domain is of type AF_INET or AF_INET6. As of now we work with AF_INET only
-          type  (required) -> Type is of type SOCK_STREAM or SOCK_DGRAM. As of now we work only with SOCK_STREAM
-          name (required)  -> Connect to an IP addresss, such as "192.168.82.45"
-          port (required)  -> Port  number to connect to . e.g. 80
-
- * @param OS_NetworkSocket_Handle_t: Handle will be filled by after the call to OS_NetworkSocket_create().
-
+ * @details Create a socket and connect to it
+ *
+ * @param[in] ctx:  Runtime context of the APP. As of now
+ * NULL.
+ *
+ * @param[in] pClientStruct: Configuration parameters for the socket to
+ * be created.
+ *
+ * @param[out] phandle: Handle to the created socket.
+ *
  * @return  SEOS_SUCCESS or SEOS_ERROR
  */
 
 seos_err_t
 OS_NetworkSocket_create(
-    OS_Network_Context_t      ctx,
+    OS_Network_Context_t       ctx,
     OS_Network_Socket_t*       pClientStruct,
     OS_NetworkSocket_Handle_t* phandle);
 
 /**
- * @details %OS_NetworkServerSocket_create, Create a server socket, binds to a port and listen for incoming connections
-
- * @param OS_Network_Context_t: Passed by App which is the run time context received by APP.
-
- * @param OS_NetworkServer_Socket_t: Must be filled by app. Please see the struct OS_NetworkServer_Socket_t for details
-
-          domain(required)  -> Domain is of type AF_INET or AF_INET6. As of now we work with AF_INET only
-          type  (required)  -> Type is of type SOCK_STREAM or SOCK_DGRAM. As of now we work only with SOCK_STREAM
-          backlog(required) -> Indicates how many incoming connections is possible (for now it is only 1 connection)
-                               Set to 1
-          port  (required)  -> Port to which you want to bind. e.g. 5555
-
- * @param OS_NetworkServer_Handle_t: Handle will be filled by after the call to OS_NetworkServerSocket_create.
+ * @details Create a server socket, binds to a
+ * port and listen for incoming connections
+ *
+ * @param[in] ctx:  Runtime context of the APP. As of now
+ * NULL.
+ *
+ * @param[in] pServerStruct: Configuration parameters for the
+ * server socket to be created.
+ *
+ * @param[out] pSrvHandle: Handle to the created socket.
  *
  * @return  SEOS_SUCCESS or SEOS_ERROR
  */
 
 seos_err_t
 OS_NetworkServerSocket_create(
-    OS_Network_Context_t      ctx,
+    OS_Network_Context_t       ctx,
     OS_NetworkServer_Socket_t* pServerStruct,
     OS_NetworkServer_Handle_t* pSrvHandle);
 /**
- * @details %OS_NetworkServerSocket_close, Must be called when the server app wants to close a comnection.
-          No further socket communication is possible after closure.
-
- * @param OS_NetworkServer_Handle_t: Handle that was used to create a server socket
-
+ * @details Closes a NetworkServer socket. No
+ * further socket communication is possible after closure.
+ *
+ * @param[in] srvHandle: Handle to the server socket we want
+ * to close.
+ *
  * @return  SEOS_SUCCESS or SEOS_ERROR
  */
 
@@ -124,9 +163,10 @@ OS_NetworkServerSocket_close(
     OS_NetworkServer_Handle_t srvHandle);
 
 /**
- * @details %OS_NetworkSocket_close, Closes a network socket. Once the close is done no further socket communication is possible.
+ * @details Closes a network socket. No further
+ * socket communication is possible after closure.
  *
- * @param OS_NetworkSocket_Handle_t: Handle used to open/create socket.
+ * @param[in] handle: Handle to the socket we want to close.
  *
  * @return SEOS_SUCCESS or SEOS_ERROR
  *
@@ -137,59 +177,60 @@ OS_NetworkSocket_close(
     OS_NetworkSocket_Handle_t handle);
 
 /**
- * @details %OS_NetworkSocket_write, Write to a network socket. Write data to a socket after connecting.
-            Copy the data to be writtenin the CamkES App dataport.
+ * @details Writes to a connected network socket.
  *
- * @param handle: Used to create/open socket
+ * @param[in] handle: Handle to the socket to be used.
  *
- * @param buf: Buffer containing data to be written
-
- * @param len: Length of data to write
+ * @param[in] buf: Buffer containing data to be written.
  *
- * @return Actual Number of Bytes written  or SEOS_ERROR
+ * @param[in] len: Length of data to be written.
+ *
+ * @return Actual Number of Bytes written or SEOS_ERROR
  *
  */
 seos_err_t
 OS_NetworkSocket_write(
     OS_NetworkSocket_Handle_t handle,
-    const void*                buf,
-    size_t*                    len);
+    const void*               buf,
+    size_t*                   len);
 
 /**
- * @details %OS_NetworkServerSocket_accept, Accept incoming connections. This is useful when the Network stack is working as Server.
+ * @details Accept incoming connections.
  *
- * @param OS_NetworkServer_Handle_t: Handle used to create server socket.
-
- * @param OS_NetworkSocket_Handle_t*. At the end accept() will fill the handle of an incoming
-                                 connection.This handle is used for further read/write.
-
+ * @param[in] srvHandle: Handle used to create server socket.
+ *
+ * @param[out] phSocket: Network Socket containing the data
+ * needed for further communication with the connecting host. All subsequent
+ * read/write commands in this conversation need this handle.
+ *
  * @return SEOS_SUCCESS or SEOS_ERROR
  */
 
 seos_err_t
 OS_NetworkServerSocket_accept(
-    OS_NetworkServer_Handle_t srvHandle,
+    OS_NetworkServer_Handle_t  srvHandle,
     OS_NetworkSocket_Handle_t* phSocket);
 
 /**
- * @details %OS_NetworkSocket_read, Read data from connected socket.
+ * @details Read data from connected socket.
 
- * @param handle: Used to create/open socket
+ * @param[in] handle: Used to create/open socket
 
- * @param buf: Buffer to read data into
+ * @param[in] buf: Buffer to read data into
  *
- * @param len: Indicates how much data to read. After read it indicates how much was actually read
+ * @param[in,out] len: Indicates how much data to read. After read it indicates how much was actually read
  *
- * @return seos_err_t, following are the values
-                       SEOS_ERROR_CONNECTION_CLOSED and length = 0, end of data and connection close
-                       SEOS_ERROR_GENERIC  due to error in read operation
-                       SEOS_SUCCESS and length = 0, nothing to read but there is a connection
-                       SEOS_SUCCESS and length > 0, data to read and there is connection
+ * @return seos_err_t, following combinations with value of len
 
+ * SEOS_ERROR_CONNECTION_CLOSED and length = 0, connection closed\n
+ * SEOS_ERROR_GENERIC  error during read\n
+ * SEOS_SUCCESS and len = 0, nothing read, connection still established\n
+ * SEOS_SUCCESS and len > 0, data read, connection still established\n
  */
 
 seos_err_t
 OS_NetworkSocket_read(
     OS_NetworkSocket_Handle_t handle,
-    void*                      buf,
-    size_t*                    len);
+    void*                     buf,
+    size_t*                   len);
+/** @} */
