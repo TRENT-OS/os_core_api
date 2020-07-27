e8264072a0e34d7a2d1054281db11308dcebb5fb
Thomas Böhm <thomas.boehm@hensoldt-cyber.com>
2020-07-27 20:09:51 +0200

    [DEV] SEOS-1541 Set struct to volatile

--- include/network/OS_NetworkStack.h
+++ include/network/OS_NetworkStack.h
@@ -10,9 +10,16 @@
 #include <stddef.h>
 #include <limits.h>
 
+// This struct is currently used to synchronize between network driver and the
+// network stack. When the len is set to 0 it means the frame stored in data
+// has been processed and the buffer can be reused. If len is non-zero it
+// indicates the length of the data stored in the buffer, which will be used
+// by the network stack.
+//
+// TODO: Make data also volatile, see SEOS-1603
 
 typedef struct
 {
-    uint8_t data[PAGE_SIZE - 16];
-    size_t  len;
+    uint8_t         data[PAGE_SIZE - 16];
+    volatile size_t len;
 } OS_NetworkStack_RxBuffer_t;
