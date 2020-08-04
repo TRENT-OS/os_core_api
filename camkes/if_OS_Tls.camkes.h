ac8fac92af7412e259a2c098d881474d8417fe31
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-08-04 16:48:47 +0200

    [DEV] Split TLS CAmkES interface into C define and CAmkES procedure

---
+++ camkes/if_OS_Tls.camkes.h
@@ -0,0 +1,13 @@
+/* Copyright (C) 2019-2020, Hensoldt Cyber GmbH */
+
+#pragma once
+
+#define IF_OS_TLS_CAMKES        \
+    OS_Error_t handshake();     \
+    OS_Error_t write(           \
+        in size_t dataSize      \
+    );                          \
+    OS_Error_t read(            \
+        inout size_t dataSize   \
+    );                          \
+    OS_Error_t reset();
