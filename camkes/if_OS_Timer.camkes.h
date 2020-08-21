a954bb6b3fb7264e63a3c0d0a19c7fc81353dcbf
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-08-21 11:57:08 +0200

    [DEV] Split Timer CAmkES interface into C define and CAmkES procedure

---
+++ camkes/if_OS_Timer.camkes.h
@@ -0,0 +1,31 @@
+/* Copyright (C) 2020, Hensoldt Cyber GmbH */
+
+#pragma once
+
+#define IF_OS_TIMER_CAMKES  \
+    OS_Error_t              \
+    completed(              \
+        out uint32_t tmr);  \
+    \
+    OS_Error_t              \
+    periodic(               \
+        in int tid,         \
+        in uint64_t ns);    \
+    \
+    OS_Error_t              \
+    oneshot_absolute(       \
+        in int tid,         \
+        in uint64_t ns);    \
+    \
+    OS_Error_t              \
+    oneshot_relative(       \
+        in int tid,         \
+        in uint64_t ns);    \
+    \
+    OS_Error_t              \
+    stop(                   \
+        in int tid);        \
+    \
+    OS_Error_t              \
+    time(                   \
+        out uint64_t ns);
