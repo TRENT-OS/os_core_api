58313d26b572edf919970daa6a859e78468f1c9d
Benedikt Driessen <benedikt.driessen.external@hensoldt-cyber.com>
2020-08-26 15:59:32 +0200

    [NEW] Add C struct of Timer CAmkES interface and assignment macro

---
+++ include/interfaces/if_OS_Timer.h
@@ -0,0 +1,30 @@
+/* Copyright (C) 2019-2020, Hensoldt Cyber GmbH */
+
+#pragma once
+
+#include "OS_Error.h"
+
+#include <stdint.h>
+#include <camkes.h>
+
+typedef struct
+{
+    OS_Error_t (*completed)(uint32_t* tmr);
+    OS_Error_t (*periodic)(int tid, uint64_t ns);
+    OS_Error_t (*oneshot_absolute)(int tid, uint64_t ns);
+    OS_Error_t (*oneshot_relative)(int tid, uint64_t ns);
+    OS_Error_t (*stop)(int tid);
+    OS_Error_t (*time)(uint64_t* ns);
+    void (*notify_wait)(void);
+} if_OS_Timer_t;
+
+#define IF_OS_TIMER_ASSIGN(_rpc_, _evt_)                \
+{                                                       \
+    .completed          = _rpc_ ## _completed,          \
+    .periodic           = _rpc_ ## _periodic,           \
+    .oneshot_absolute   = _rpc_ ## _oneshot_absolute,   \
+    .oneshot_relative   = _rpc_ ## _oneshot_relative,   \
+    .stop               = _rpc_ ## _stop,               \
+    .time               = _rpc_ ## _time,               \
+    .notify_wait        = _evt_ ## _wait,               \
+}
